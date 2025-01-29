fa859cef3fb71e688a9b57271a9b3b01c4ad02dc
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-01-29 21:13:19 +0530

    com

--- Module/index.js
+++ Module/index.js
@@ -12,4 +12,35 @@ const rl = readline.createInterface({
 
 let currentDir = process.cwd();
 
+
+
 console.log(`Current Directory : ${currentDir}`);
+
+function prompt(){
+    rl.question("Enter command (ls, cat <file>, write <path> <file> <content>, cd <dir>, cat <file>, exit): ", (input)=>{
+        const args = input.split(" ");
+        const command = args[0];
+
+        switch(command){
+            case 'ls':
+                listFiles(currentDir);
+                break;
+            case 'cat':
+                if(args[1]) readFile(currentDir, args[1]);
+                else console.log("Please specify a file to read");
+                break;
+            case 'write':
+                if(args[1] && args[2] && args[3]) writeFile(args[1], args[2], args.slice(3).join(" "));
+                else console.log("Usage: write <path> <file> <content>");
+            case 'cd':
+                if(args[1]) currentDir = changeDir(currentDir, args[1]);
+                else console.log("Please specify a directory");
+                break;
+            case 'exit':
+                rl.close();
+                return;
+            default:
+                console.log("Unknown Command");
+        }
+    })
+}
