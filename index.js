b9891cca826152c421804a832ef58c9de4f30f6b
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-01-31 21:25:08 +0530

    com

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res)=>{
    res.end("Hello world !!!")
 })
 
+app.get('/about', (req, res)=>{
+    res.status
+   res.send("<h1>Hello from about page !!!</h1>")
+})
+
 app.listen(PORT, ()=>{
     console.log(`listening on express server on port ${PORT}`);
 
